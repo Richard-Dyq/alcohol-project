44c6c8e1c599fa4e6f586c621484dae0154f8ccf
yuanyuandeng <yn307233@dal.ca>
2019-06-10 14:43:19 -0300

    Yuanyuan Deng & Zihao Liu finished transaction button to form page

--- app/src/main/java/com/csci3130/project/team1/MainActivity.java
+++ app/src/main/java/com/csci3130/project/team1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.csci3130.project.team1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ecl_button = (Button) this.findViewById(R.id.ecl_button);
+
+        ecl_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ECL_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
