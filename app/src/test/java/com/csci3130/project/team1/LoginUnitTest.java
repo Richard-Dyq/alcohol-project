a3ebccfe417ca9409c4f1feab1ddbf4f6f601aff
Yizhao He <yz855856@dal.ca>
2019-06-12 16:52:16 -0300

    Yizhao He and ZiXuan Liu finished test for isEmailANDpasswdvalid Unit Test

---
+++ app/src/test/java/com/csci3130/project/team1/LoginUnitTest.java
@@ -0,0 +1,18 @@
+package com.csci3130.project.team1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginUnitTest {
+    @Test
+    public void isUserNameAndPasswordValid_iscorrect() {
+        LoginActivity loginActivity = new LoginActivity();
+
+        assertTrue(loginActivity.isUserNameAndPasswordValid("james@dal.ca", "123333"));
+        assertFalse(loginActivity.isUserNameAndPasswordValid("dal.ca", "123333"));
+        assertFalse(loginActivity.isUserNameAndPasswordValid("james@dal.ca", ""));
+    }
+}
