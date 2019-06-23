20e52ed8ba24d8b6d7d3abde1d5e7f8e5fe166ea
Yizhao He <yz855856@dal.ca>
2019-06-23 17:43:09 -0300

    Yizhao He & Jiutian Zhang finished Test for cannabis viewing list by date

---
+++ app/src/test/java/com/csci3130/project/team1/CannabisModelRepresentationTest.java
@@ -0,0 +1,35 @@
+package com.csci3130.project.team1;
+
+import android.os.Bundle;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class CannabisModelRepresentationTest {
+    @Test
+    public void getCannaisLevelRep_isCorrect() {
+        CannabisIntake cannabisIntake = new CannabisIntake(12, new Date());
+        String level_rep = cannabisIntake.getC_level_Literal();
+        assertTrue(level_rep.contains("LEVEL:"));
+    }
+
+    @Test
+    public void getCannaisDateRep_isCorrect() {
+        CannabisIntake cannabisIntake = new CannabisIntake(12, new Date());
+        assertTrue(cannabisIntake.getDate_literal().matches("\\d{4}-\\d{2}-\\d{2}"));
+    }
+
+}
