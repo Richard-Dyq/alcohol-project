0b3a46d7726869c7f64a52d5559765d596163714
yuanyuandeng <yn307233@dal.ca>
2019-07-10 13:22:28 -0300

    JiuTian Zhang, Yuanyuan Deng finished Cannabis model

--- app/src/main/java/com/csci3130/project/team1/model/CannabisProduct.java
+++ app/src/main/java/com/csci3130/project/team1/model/CannabisProduct.java
@@ -1,4 +1,37 @@
 package com.csci3130.project.team1.model;
 
 public class CannabisProduct {
+    private String name;
+    private String brand;
+    private double THC;
+
+    public CannabisProduct(String name, String brand, double THC) {
+        this.name = name;
+        this.brand = brand;
+        this.THC = THC;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getTHC() {
+        return THC;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setTHC(double THC) {
+        this.THC = THC;
+    }
 }
